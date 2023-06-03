94d206b09e1bb9abaea3b49586b394b87ee5893d
dzungvpham <vietdung113911@gmail.com>
2023-06-03 21:21:37 +0000

    Add neural pdgd

--- code/ranker.py
+++ code/ranker.py
@@ -1,6 +1,7 @@
 import itertools
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.func import grad, vmap
 
 class BaseRanker:
@@ -38,7 +39,7 @@ class BasePDGDRanker(BaseRanker):
         log_prob_swapped = vmap(self.log_ranking_prob, in_dims=(0, None))(flipped_rankings, fx)
         log_prob_swapped_mtx = torch.zeros(num_items, num_items)
         log_prob_swapped_mtx[torch.triu_indices(num_items, num_items, offset=1).tolist()] = log_prob_swapped
-        log_prob_swapped_mtx[torch.tril_indices(num_items, num_items, offset=-1).tolist()] = log_prob_swapped
+        log_prob_swapped_mtx = log_prob_swapped_mtx + log_prob_swapped_mtx.t()
 
         log_prob_orig = self.log_ranking_prob(ranking, fx)
         return log_prob_swapped_mtx - torch.logaddexp(log_prob_orig, log_prob_swapped_mtx)
@@ -69,6 +70,51 @@ class LinearPDGDRanker(BasePDGDRanker):
     def forward(self, params, features):
         return params.dot(features)
     
+class Neural1LayerPDGDRanker(BasePDGDRanker):
+    def __init__(self, feature_size, hidden_size, activation):
+        self.feature_size = feature_size
+        self.hidden_size = hidden_size
+        self.activation = activation
+        
+    def forward(self, params, features):
+        num_hidden_features = self.feature_size * self.hidden_size
+        hidden_params = params[:num_hidden_features].reshape(self.hidden_size, -1).t()
+        res = self.activation(features @ hidden_params)
+        return params[num_hidden_features:].dot(res)
+    
+class Neural2LayerPDGDRanker(BasePDGDRanker):
+    def __init__(self, feature_size, hidden_size, hidden_size2, activation):
+        self.feature_size = feature_size
+        self.hidden_size = hidden_size
+        self.hidden_size2 = hidden_size2
+        self.activation = activation
+        
+    def forward(self, params, features):
+        num_hidden_features = self.feature_size * self.hidden_size
+        num_hidden_features2 = self.hidden_size * self.hidden_size2
+        num_hidden_features_all = num_hidden_features + num_hidden_features2
+        
+        hidden_params = params[:num_hidden_features].reshape(self.hidden_size, -1).t()
+        hidden_params2 = params[num_hidden_features:num_hidden_features_all].reshape(self.hidden_size2, -1).t()
+        
+        res = self.activation(features @ hidden_params)
+        res2 = self.activation(res @ hidden_params2)
+        return params[num_hidden_features_all:].dot(res2)
+    
 if __name__ == "__main__":
+    num_features = 5
+    num_data = 3
+    X = torch.rand(num_data, num_features)
+    ranking = torch.LongTensor([1, 0, 2])
+    interactions = torch.Tensor([1, 0, 1]).reshape(-1, 1)
+    
     ranker = LinearPDGDRanker()
-    print(ranker.grad(torch.rand(5), torch.rand(3, 5), torch.LongTensor([1, 0, 2]), torch.Tensor([1, 0, 1]).reshape(-1, 1)))
+    print(ranker.grad(torch.rand(num_features), X, ranking, interactions))
+    
+    hidden_size = 2
+    ranker2 = Neural1LayerPDGDRanker(num_features, hidden_size, F.relu)
+    print(ranker2.grad(torch.rand((num_features + 1) * hidden_size), X, ranking, interactions))
+    
+    hidden_size2 = 2
+    ranker3 = Neural2LayerPDGDRanker(num_features, hidden_size, hidden_size2, F.relu)
+    print(ranker3.grad(torch.rand(num_features * hidden_size + hidden_size * hidden_size2 + hidden_size2), X, ranking, interactions))
