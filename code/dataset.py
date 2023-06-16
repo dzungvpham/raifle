ecb34146c3635d41396326aa225ddb1997036798
Dzung Pham <dzungvpham@gmail.com>
2023-06-16 10:30:22 -0400

    Add LTR dataset and some typings

--- code/dataset.py
+++ code/dataset.py
@@ -1,6 +1,50 @@
 import numpy as np
 import os.path
 import pandas as pd
+from tqdm import tqdm
+from typing import Dict, List, Tuple
+
+
+class LearningToRankDataset:
+    def __init__(self, path=""):
+        print(f"Loading {path}")
+
+        rows = []
+        with open(path, "r") as file:
+            for line in tqdm(file):
+                line_parts = line.split(" ")
+                row = {
+                    "qid": int(line_parts[1].split(":")[1]),
+                    "relevance": int(line_parts[0]),
+                }
+
+                for col in line_parts[2:]:
+                    if col == "#docid":
+                        break
+                    if ":" not in col:
+                        continue
+
+                    feature, feature_val = col.split(":")
+                    row[int(feature)] = float(feature_val)
+
+                rows.append(row)
+
+        df = pd.DataFrame(rows)
+        self.qids = df["qid"].unique().tolist()
+        self.qid_to_data_map = df.groupby("qid").apply(
+            lambda x: (x["relevance"].values.tolist(), x.iloc[:, 2:].values.tolist())
+        )
+        self.num_features = len(df.columns) - 2
+
+    def get_all_query_ids(self) -> List:
+        return self.qids
+    
+    # Returns an array of tuple of the form (relevances, features)
+    def get_data_for_queries(self, qids: List) -> List[Tuple[List, List]]:
+        return self.qid_to_data_map.loc[qids].tolist()
+
+    def get_num_features(self) -> int:
+        return self.num_features
 
 
 class RecommendationDataset:
@@ -11,33 +55,28 @@ class RecommendationDataset:
             lambda x: dict(zip(x[item_id_col], x[rating_col]))
         )
 
-    def get_all_user_ids(self) -> list:
+    def get_all_user_ids(self) -> List:
         return self.user_ids
 
-    def get_num_users(self):
-        return len(self.user_ids)
-
-    def get_all_item_ids(self) -> list:
+    def get_all_item_ids(self) -> List:
         return self.item_ids
 
-    def get_num_items(self):
-        return len(self.item_ids)
-
-    def get_item_ids_for_users(self, user_ids: list):
+    def get_item_ids_for_users(self, user_ids: List) -> List[List]:
         return (
             self.user_to_item_rating_map.loc[user_ids]
             .apply(lambda x: list(x.keys()))
             .tolist()
         )
 
-    def get_non_interacted_item_ids_for_users(self, user_ids: list):
+    def get_non_interacted_item_ids_for_users(self, user_ids: List) -> List[List]:
         return (
             self.user_to_item_rating_map.loc[user_ids]
             .apply(lambda x: list(set(self.get_all_item_ids()) - set(x.keys())))
             .tolist()
         )
 
-    def get_item_ratings_for_users(self, user_ids: list):
+    # Return a list of dictionary from item id to rating
+    def get_item_ratings_for_users(self, user_ids: List) -> List[Dict]:
         return self.user_to_item_rating_map.loc[user_ids].tolist()
 
 
@@ -74,9 +113,10 @@ class Yelp(RecommendationDataset):
         if cache_path is not None and os.path.isfile(cache_path):
             df = pd.read_csv(cache_path)
         else:  # Preprocess
+            print(f"Processing {path}")
             reader = pd.read_json(path, lines=True, chunksize=10000)
             df = pd.DataFrame()
-            for chunk in reader:
+            for chunk in tqdm(reader):
                 df = pd.concat([df, chunk[["user_id", "business_id", "stars"]]])
             df["user_id"], _ = pd.factorize(df["user_id"])
             df["business_id"], _ = pd.factorize(df["business_id"])
